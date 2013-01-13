33a9abfc3efdca891bc544d098c6cac1c8630cf7
Person8880 <person8880@googlemail.com>
2013-01-13 20:26:06 +0000

    Fixed some more bugs in the hook system
    Also accounted for NS2Stat's odd way of overriding a certain event so
    that Shine's hook runs properly.

--- lua/extensions/badges.lua
+++ lua/extensions/badges.lua
@@ -8,74 +8,78 @@ local Plugin = {}
 Plugin.Version = "1.0"
 
 function Plugin:Initialise()
-	local BadgeMixin = BadgeMixin
+	Shine.Hook.Add( "Think", "ReplaceBadges", function( Deltatime )
+		local BadgeMixin = BadgeMixin
 
-	if not BadgeMixin then 
-		return false, "Badge mixin not found."
-	end
-
-	local OldInitBadge
-	local GetBadge = BadgeMixin.GetBadgeIcon
-
-	local BadgeData = Shine.GetUpValue( GetBadge, "kBadgeData" )
-	if not BadgeData then 
-		return false, "Badge data not found."
-	end
-
-	local function BadgeAuthorized( Client, BadgeID )
-		local UserData = Shine.UserData
-
-		local SteamID = Client:GetUserId()
-
-		if type( BadgeID ) == "number" then
-			return Server.GetIsDlcAuthorized( Client, BadgeID )
-		elseif UserData then
-			if not UserData.Users then return false end
-			local User = UserData.Users[ tostring( SteamID ) ]
-
-			if User and User.Group == BadgeID then return true end
+		if not BadgeMixin then 
+			return false, "Badge mixin not found."
 		end
-		
-		return false
-	end
 
-	local BadgeCache = {}
-	local function CacheGet( Client )
-		local SteamID = Client:GetUserId()
-		local Badge = BadgeCache[ SteamID ]
+		local OldInitBadge
+		local GetBadge = BadgeMixin.GetBadgeIcon
 
-		if Badge then
+		local BadgeData = Shine.GetUpValue( GetBadge, "kBadgeData" )
+		if not BadgeData then 
+			return false, "Badge data not found."
+		end
+
+		local function BadgeAuthorized( Client, BadgeID )
+			local UserData = Shine.UserData
+
+			local SteamID = Client:GetUserId()
+
+			if type( BadgeID ) == "number" then
+				return Server.GetIsDlcAuthorized( Client, BadgeID )
+			elseif UserData then
+				if not UserData.Users then return false end
+				local User = UserData.Users[ tostring( SteamID ) ]
+
+				if User and User.Group == BadgeID then return true end
+			end
+			
+			return false
+		end
+
+		local BadgeCache = {}
+		local function CacheGet( Client )
+			local SteamID = Client:GetUserId()
+			local Badge = BadgeCache[ SteamID ]
+
+			if Badge then
+				return Badge
+			end
+
+			Shared.SendHTTPRequest( "http://ns2comp.herokuapp.com/t/badge/"..tostring( SteamID ), "GET", function( Response )
+				local Data = Decode( Response )
+				
+				if Data.override or not Badge or Badge == kBadges.None or Badge == kBadges.PAX2012 then
+					BadgeCache[ SteamID ] = kBadges[ Data.badge ]
+				end
+			end )
+
+			Badge = kBadges.None
+
+			for Enum, Data in pairs( BadgeData ) do
+				if Data.Id and BadgeAuthorized( Client, Data.Id ) then
+					Badge = Enum
+					break
+				end
+			end
+
+			BadgeCache[ SteamID ] = Badge
+
 			return Badge
 		end
 
-		Shared.SendHTTPRequest( "http://ns2comp.herokuapp.com/t/badge/"..tostring( SteamID ), "GET", function( Response )
-			local Data = Decode( Response )
-			
-			if Data.override or not Badge or Badge == kBadges.None or Badge == kBadges.PAX2012 then
-				BadgeCache[ SteamID ] = kBadges[ Data.badge ]
+		OldInitBadge = Shine.ReplaceClassMethod( "BadgeMixin", "InitializeBadges", function( Mixin )
+			local Client = Server.GetOwner( Mixin )
+
+			if Client then
+				Mixin:SetBadge( CacheGet( Client ) )
 			end
 		end )
 
-		Badge = kBadges.None
-
-		for Enum, Data in pairs( BadgeData ) do
-			if Data.Id and BadgeAuthorized( Client, Data.Id ) then
-				Badge = Enum
-				break
-			end
-		end
-
-		BadgeCache[ SteamID ] = Badge
-
-		return Badge
-	end
-
-	OldInitBadge = Shine.ReplaceClassMethod( "BadgeMixin", "InitializeBadges", function( Mixin )
-		local Client = Server.GetOwner( Mixin )
-
-		if Client then
-			Mixin:SetBadge( CacheGet( Client ) )
-		end
+		Shine.Hook.Remove( "Think", "ReplaceBadges" )
 	end )
 
 	self.Enabled = true
