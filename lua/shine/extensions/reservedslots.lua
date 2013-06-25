a36ace546b65f4c51ba5062e72180700743ac890
Person8880 <person8880@googlemail.com>
2013-06-25 20:03:38 +0100

    Redirect method for reserved slots
    If a player joins a reserved slot, they are redirected to another
    server.

--- lua/shine/extensions/reservedslots.lua
+++ lua/shine/extensions/reservedslots.lua
@@ -6,6 +6,8 @@
 
 local Shine = Shine
 
+local Clamp = math.Clamp
+local Floor = math.floor
 local Max = math.max
 local Notify = Shared.Message
 local StringFormat = string.format
@@ -17,53 +19,24 @@ Plugin.Version = "1.0"
 Plugin.HasConfig = true
 Plugin.ConfigName = "ReservedSlots.json"
 
+Plugin.MODE_PASSWORD = 1
+Plugin.MODE_REDIRECT = 2
+
+Plugin.DefaultConfig = {
+	Slots = 2,
+	Password = "",
+	Mode = 1,
+	Redirect = { IP = "127.0.0.1", Port = "27015", Password = "" }
+}
+
+Plugin.CheckConfig = true
+
 function Plugin:Initialise()
+	self.Config.Mode = Clamp( Floor( self.Config.Mode ), 1, 2 )
+
 	self.Enabled = true
 
 	return true
-end
-
-function Plugin:GenerateDefaultConfig( Save )
-	self.Config = {
-		Slots = 2,
-		Password = ""
-	}
-
-	if Save then
-		local Success, Err = Shine.SaveJSONFile( self.Config, Shine.Config.ExtensionDir..self.ConfigName )
-
-		if not Success then
-			Notify( "Error writing reservedslots config file: "..Err )	
-
-			return	
-		end
-
-		Notify( "Shine reservedslots config file created." )
-	end
-end
-
-function Plugin:SaveConfig()
-	local Success, Err = Shine.SaveJSONFile( self.Config, Shine.Config.ExtensionDir..self.ConfigName )
-
-	if not Success then
-		Notify( "Error writing reservedslots config file: "..Err )	
-
-		return	
-	end
-
-	Notify( "Shine reservedslots config file updated." )
-end
-
-function Plugin:LoadConfig()
-	local PluginConfig = Shine.LoadJSONFile( Shine.Config.ExtensionDir..self.ConfigName )
-
-	if not PluginConfig then
-		self:GenerateDefaultConfig( true )
-
-		return
-	end
-
-	self.Config = PluginConfig
 end
 
 function Plugin:LockServer( Unlock )
@@ -71,6 +44,50 @@ function Plugin:LockServer( Unlock )
 
 	Server.SetPassword( Unlock and "" or self.Config.Password )
 end
+
+local OnReservedConnect = {
+	function( self, Client, Connected, MaxPlayers )
+		if not self.Locked then
+			Shine:LogString( StringFormat( "[Reserved Slots] Locking the server at %i/%i players.", Connected, MaxPlayers ) )
+			self:LockServer()
+		end
+	end,
+
+	function( self, Client )
+		local Redirect = self.Config.Redirect
+
+		local IP = Redirect.IP
+		local Port = Redirect.Port
+		local Password = ""
+
+		if Redirect.Password and #Redirect.Password > 0 then
+			Password = " "..Redirect.Password
+		end
+
+		Shine:NotifyColour( Client, 255, 255, 0, "This server is full, you will be redirected to one of our other servers." )
+
+		Shine.Timer.Simple( 20, function()
+			if Shine:IsValidClient( Client ) then
+				Server.SendNetworkMessage( Client, "Shine_Command", { 
+					Command = StringFormat( "connect %s:%s%s", IP, Port, Password )
+				}, true )
+			end
+		end )
+	end
+}
+
+local OnReservedDisconnect = {
+	function( self, Client, Connected, MaxPlayers )
+		if self.Locked then
+			Shine:LogString( StringFormat( "[Reserved Slots] Unlocking the server at %i/%i players.", Connected, MaxPlayers ) )
+			self:LockServer( true )
+		end
+	end,
+
+	function( self, Client )
+		--Do nothing...
+	end
+}
 
 function Plugin:ClientConnect( Client )
 	local MaxPlayers = Server.GetMaxPlayers()
@@ -83,9 +100,8 @@ function Plugin:ClientConnect( Client )
 
 	local Connected = TableCount( Shine.GameIDs )
 
-	if MaxPlayers - Slots == Connected and not self.Locked then
-		Shine:LogString( StringFormat( "[Reserved Slots] Locking the server at %i/%i players.", Connected, MaxPlayers ) )
-		self:LockServer()
+	if MaxPlayers - Slots <= Connected then
+		OnReservedConnect[ self.Config.Mode ]( self, Client, Connected, MaxPlayers )
 	end
 end
 
@@ -97,9 +113,8 @@ function Plugin:ClientDisconnect( Client )
 	local Slots = Max( self.Config.Slots - Count, 0 )
 	local Connected = TableCount( Shine.GameIDs )
 
-	if MaxPlayers - Slots > Connected and self.Locked then
-		Shine:LogString( StringFormat( "[Reserved Slots] Unlocking the server at %i/%i players.", Connected, MaxPlayers ) )
-		self:LockServer( true )
+	if MaxPlayers - Slots > Connected then
+		OnReservedDisconnect[ self.Config.Mode ]( self, Client, Connected, MaxPlayers )
 	end
 end
 
