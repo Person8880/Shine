5db49c197e849eee9b7968103553edd5847bacc1
Person8880 <person8880@googlemail.com>
2013-08-05 16:36:18 +0100

    Slight change in server switch
    Should only add/edit the votemenu pages once, not in Initialise in case
    the plugin is reloaded.

--- lua/shine/extensions/serverswitch/shared.lua
+++ lua/shine/extensions/serverswitch/shared.lua
@@ -17,6 +17,7 @@ if Server then return end
 
 local Shine = Shine
 local SGUI = Shine.GUI
+local VoteMenu = Shine.VoteMenu
 
 local Ceil = math.ceil
 local StringFormat = string.format
@@ -29,50 +30,48 @@ Plugin.ServerList = {}
 local ZeroVec = Vector( 0, 0, 0 )
 
 function Plugin:Initialise()
-	local VoteMenu = Shine.VoteMenu
-
-	VoteMenu:AddPage( "ServerSwitch", function( self )
-		self:AddBottomButton( "Back", function()
-			self:SetPage( "Main" )
-		end )
-
-		local Servers = Plugin.ServerList
-
-		local function ClickServer( ID )
-			if self.GetCanSendVote() then
-				Shared.ConsoleCommand( "sh_switchserver "..ID )
-
-				return true
-			end
-
-			return false
-		end
-
-		for ID, Server in pairs( Servers ) do
-			local Button = self:AddSideButton( Server.Name, function() ClickServer( ID ) end )
-
-			Shine.QueryServerPopulation( Server.IP, tonumber( Server.Port ) + 1, function( Connected, Max )
-				if not Connected then return end
-				if not SGUI.IsValid( Button ) then return end
-				if Button:GetText() ~= Server.Name then return end
-
-				Button:SetText( StringFormat( "%s (%i/%i)", Server.Name, Connected, Max ) )
-			end )
-		end
-	end )
-
-	VoteMenu:EditPage( "Main", function( self )
-		if next( Plugin.ServerList ) then
-			self:AddBottomButton( "Switch Server", function()
-				self:SetPage( "ServerSwitch" )
-			end )
-		end
-	end )
-
 	self.Enabled = true
 
 	return true
 end
+
+VoteMenu:AddPage( "ServerSwitch", function( self )
+	self:AddBottomButton( "Back", function()
+		self:SetPage( "Main" )
+	end )
+
+	local Servers = Plugin.ServerList
+
+	local function ClickServer( ID )
+		if self.GetCanSendVote() then
+			Shared.ConsoleCommand( "sh_switchserver "..ID )
+
+			return true
+		end
+
+		return false
+	end
+
+	for ID, Server in pairs( Servers ) do
+		local Button = self:AddSideButton( Server.Name, function() ClickServer( ID ) end )
+
+		Shine.QueryServerPopulation( Server.IP, tonumber( Server.Port ) + 1, function( Connected, Max )
+			if not Connected then return end
+			if not SGUI.IsValid( Button ) then return end
+			if Button:GetText() ~= Server.Name then return end
+
+			Button:SetText( StringFormat( "%s (%i/%i)", Server.Name, Connected, Max ) )
+		end )
+	end
+end )
+
+VoteMenu:EditPage( "Main", function( self )
+	if next( Plugin.ServerList ) then
+		self:AddBottomButton( "Switch Server", function()
+			self:SetPage( "ServerSwitch" )
+		end )
+	end
+end )
 
 Client.HookNetworkMessage( "Shine_SendServerList", function( Data )
 	if Plugin.ServerList[ Data.ID ] then --We're refreshing the data.
