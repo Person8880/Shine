666e81966858bccd1fba49be6bcfe0770586d4c4
Person8880 <person8880@googlemail.com>
2013-09-23 14:27:15 +0100

    Better function name
    I kinda broke my own styling guidelines there...

--- lua/shine/extensions/mapvote/server.lua
+++ lua/shine/extensions/mapvote/server.lua
@@ -83,7 +83,7 @@ Plugin.NextMapTimer = "MapVoteNext"
 
 local IsType = Shine.IsType
 
-local function isarray( Table )
+local function IsTableArray( Table )
 	local Count = #Table
 	return Count > 0 and Count or nil
 end
@@ -175,7 +175,7 @@ function Plugin:Initialise()
 	self.MapCycle.time = self.MapCycle.time or 30
 
 	local ForcedMaps = self.Config.ForcedMaps
-	local IsArray = isarray( ForcedMaps )
+	local IsArray = IsTableArray( ForcedMaps )
 	local MaxOptions = self.Config.MaxOptions
 
 	if IsArray then
@@ -190,7 +190,7 @@ function Plugin:Initialise()
 	end
 
 	local DontExtend = self.Config.DontExtend
-	IsArray = isarray( DontExtend )
+	IsArray = IsTableArray( DontExtend )
 
 	if IsArray then
 		for i = 1, IsArray do
@@ -200,7 +200,7 @@ function Plugin:Initialise()
 	end
 
 	local DontAutoCycle = self.Config.IgnoreAutoCycle
-	IsArray = isarray( DontAutoCycle )
+	IsArray = IsTableArray( DontAutoCycle )
 
 	if IsArray then
 		for i = 1, IsArray do
