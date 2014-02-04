31f0b6ee0a8d1e9f23bd12f97cec1c8bdf97c8c3
Person8880 <person8880@googlemail.com>
2014-02-04 21:01:57 +0000

    Stylistic changes and a few bugs fixed
    with the round limiter plugin.
    
    Fixed it not always removing the round timer, for instance, if the round
    was reset.
    
    Also fixed it showing one too many notifications (now shows exactly
    WarningRepeatTimes messages, rather than +1).

--- lua/shine/extensions/roundlimiter.lua
+++ lua/shine/extensions/roundlimiter.lua
@@ -1,81 +1,111 @@
 --[[
-    Shine Roundlimiter Plugin
+	Shine Roundlimiter Plugin
 ]]
+
 local Shine = Shine
+
+local StringFormat = string.format
+local TimeToString = string.TimeToString
 
 local Plugin = {}
 Plugin.Version = "1.0"
 
 Plugin.HasConfig = true
-Plugin.ConfigName = "roundlimiter.json"
-Plugin.DefaultConfig =
-{
-    WarningTime = 5,
-    WarningRepeatTimes = 5,
-    MaxRoundLength = 60,
+Plugin.ConfigName = "RoundLimiter.json"
+
+Plugin.DefaultConfig = {
+	WarningTime = 5,
+	WarningRepeatTimes = 5,
+	MaxRoundLength = 60
 }
+
 Plugin.CheckConfig = true
 
 local TeamScores = {
-    [ 1 ] = 0,
-    [ 2 ] = 0,
+	[ 1 ] = 0,
+	[ 2 ] = 0,
 }
 
-Shine.Hook.SetupClassHook("ScoringMixin","AddScore","OnScore","PassivePost")
-function Plugin:OnScore(player, points, res, wasKill)
-    if not points then return end
-    
-    local teamnr = player.GetTeamNumber and player:GetTeamNumber()    
-    if teamnr ~= 1 and teamnr ~= 2 then return end
-    
-    TeamScores[teamnr] =  TeamScores[teamnr] + points   
+Shine.Hook.SetupClassHook( "ScoringMixin", "AddScore", "OnScore", "PassivePost" )
+
+--[[
+	Keep track of the playing team scores.
+]]
+function Plugin:OnScore( Player, Points, Res, WasKill )
+	if not Points then return end
+	
+	local Team = Player.GetTeamNumber and Player:GetTeamNumber()    
+	if not TeamScores[ Team ] then return end
+	
+	TeamScores[ Team ] = TeamScores[ Team ] + Points   
 end
 
+--[[
+	Ends the round, making the team with the highest tracked score win.
+]]
 function Plugin:EndRound()
-    local winner = 2
-    if TeamScores[1] > TeamScores[2] then winner = 1 end
-    
-    local Gamerules = GetGamerules()
-    if not Gamerules then return end
-    
-    Shine:NotifyDualColour(nil , 100, 255, 100, "[RoundLimiter]", 255, 255, 255,"Round ends now ...")
-    Gamerules:EndGame( winner == 2 and Gamerules.team2 or Gamerules.team1 )
+	local Winner = 2
+	if TeamScores[ 1 ] > TeamScores[ 2 ] then Winner = 1 end
+	
+	local Gamerules = GetGamerules()
+	if not Gamerules then return end
+	
+	Shine:NotifyDualColour( nil, 100, 255, 100, "[RoundLimiter]", 255, 255, 255,
+		"Ending round due to time limit..." )
+	
+	Gamerules:EndGame( Winner == 2 and Gamerules.team2 or Gamerules.team1 )
 end
 
 local WarningsLeft = 0
 
-function Plugin:WarningMsg()
-   local m = string.format("There are %i seconds left until this round ends.", WarningsLeft * self.Config.WarningTime * 60 / self.Config.WarningRepeatTimes)
-   WarningsLeft = WarningsLeft - 1
-   Shine:NotifyDualColour( nil , 100, 255, 100, "[RoundLimiter]", 255, 255, 255, m)
+function Plugin:DisplayWarning()
+	local TimeLeft = WarningsLeft * self.Config.WarningTime * 60 / self.Config.WarningRepeatTimes
+	local Message = StringFormat( "%s left until this round ends.", 
+		TimeToString( TimeLeft ) )
+
+	WarningsLeft = WarningsLeft - 1
+
+	Shine:NotifyDualColour( nil , 100, 255, 100, "[RoundLimiter]", 255, 255, 255, Message )
 end
 
-function Plugin:Warn()
-   WarningsLeft = self.Config.WarningRepeatTimes
-   Plugin:WarningMsg()
-   self:CreateTimer("Nag", self.Config.WarningTime * 60 / self.Config.WarningRepeatTimes, self.Config.WarningRepeatTimes,function() self:WarningMsg() end)
+function Plugin:StartWarning()
+	WarningsLeft = self.Config.WarningRepeatTimes
+
+	self:DisplayWarning()
+
+	if WarningsLeft > 0 then
+		local TimeInterval = self.Config.WarningTime * 60 / self.Config.WarningRepeatTimes
+
+		self:CreateTimer( "Nag", TimeInterval, WarningsLeft, function()
+			self:DisplayWarning()
+		end )
+	end
 end
 
---Gamestart
 function Plugin:SetGameState( Gamerules, NewState, OldState )    
-    if NewState == kGameState.Started then
-        
-        --reset team scores
-        TeamScores = {
-            [ 1 ] = 0,
-            [ 2 ] = 0,
-        }
-        
-        if self.Config.WarningTime > 0 then 
-            self:SimpleTimer((self.Config.MaxRoundLength - self.Config.WarningTime) * 60, function() self:Warn() end)
-        end    
-        self:SimpleTimer(self.Config.MaxRoundLength * 60, function() self:EndRound() end)
-    end
-end
+	if NewState ~= kGameState.Started then 
+		self:DestroyAllTimers()
 
---Gameend
-function Plugin:EndGame( Gamerules, WinningTeam )
-    self:DestroyAllTimers()
+		return
+	end
+
+	--Reset team scores.
+	TeamScores[ 1 ] = 0
+	TeamScores[ 2 ] = 0
+
+	--Queue the warnings.
+	if self.Config.WarningTime > 0 then
+		local WarnTime = ( self.Config.MaxRoundLength - self.Config.WarningTime ) * 60
+		
+		self:SimpleTimer( WarnTime, function()
+			self:StartWarning()
+		end )
+	end
+
+	--Queue the round end.
+	self:SimpleTimer( self.Config.MaxRoundLength * 60, function()
+		self:EndRound()
+	end )
 end
 
 Shine:RegisterExtension( "roundlimiter", Plugin )
