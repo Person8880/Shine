1fea1f2cf60435f4b6aa645c819d6d00e1454eae
Person8880 <person8880@googlemail.com>
2014-02-15 19:40:24 +0000

    Added win conditions to the round limiter plugin
    You can choose whether you want the team who scored the most points, had
    the most RTs at the end, or collected the most team resources over the
    round to win.

--- lua/shine/extensions/roundlimiter.lua
+++ lua/shine/extensions/roundlimiter.lua
@@ -10,13 +10,18 @@ local TimeToString = string.TimeToString
 local Plugin = {}
 Plugin.Version = "1.0"
 
+Plugin.WIN_SCORE = 1
+Plugin.WIN_RTS = 2
+Plugin.WIN_COLLECTEDRES = 3
+
 Plugin.HasConfig = true
 Plugin.ConfigName = "RoundLimiter.json"
 
 Plugin.DefaultConfig = {
 	WarningTime = 5,
 	WarningRepeatTimes = 5,
-	MaxRoundLength = 60
+	MaxRoundLength = 60,
+	WinCondition = 1
 }
 
 Plugin.CheckConfig = true
@@ -32,6 +37,7 @@ Shine.Hook.SetupClassHook( "ScoringMixin", "AddScore", "OnScore", "PassivePost" 
 	Keep track of the playing team scores.
 ]]
 function Plugin:OnScore( Player, Points, Res, WasKill )
+	if self.Config.WinCondition ~= self.WIN_SCORE then return end
 	if not Points then return end
 	
 	local Team = Player.GetTeamNumber and Player:GetTeamNumber()    
@@ -45,10 +51,31 @@ end
 ]]
 function Plugin:EndRound()
 	local Winner = 2
-	if TeamScores[ 1 ] > TeamScores[ 2 ] then Winner = 1 end
-	
+
 	local Gamerules = GetGamerules()
 	if not Gamerules then return end
+
+	local WinCondition = self.Config.WinCondition
+
+	--Team with the most points scored over the game.
+	if WinCondition == self.WIN_SCORE then
+		if TeamScores[ 1 ] > TeamScores[ 2 ] then Winner = 1 end
+	--Team with the most RTs at the time of ending.
+	elseif WinCondition == self.WIN_RTS then
+		local Extractors = Shared.GetEntitiesWithClassname( "Extractor" ):GetSize()
+		local Harvesters = Shared.GetEntitiesWithClassname( "Harvester" ):GetSize()
+
+		if Extractors > Harvesters then Winner = 1 end
+	--Team that collected the most team resources (i.e resources over the whole game).
+	elseif WinCondition == self.WIN_COLLECTEDRES then
+		local Marines = Gamerules.team1
+		local Aliens = Gamerules.team2
+
+		local MarineRes = Marines:GetTotalTeamResources()
+		local AlienRes = Aliens:GetTotalTeamResources()
+
+		if MarineRes > AlienRes then Winner = 1 end
+	end
 	
 	Shine:NotifyDualColour( nil, 100, 255, 100, "[RoundLimiter]", 255, 255, 255,
 		"Ending round due to time limit..." )
