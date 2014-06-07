843cb16d2dca3e31180f9f01081c630fff47cde9
Person8880 <person8880@googlemail.com>
2014-06-07 13:44:29 +0100

    Remove server browser fix
    It's now in 266.

--- lua/shine/init.lua
+++ lua/shine/init.lua
@@ -60,21 +60,3 @@ if Shine.Error then
 end
 
 Shine:Print( "Shine started up successfully." )
-
---Assuming they'll fix it in 266 now they know what it is...
-if Shared.GetBuildNumber() > 265 then return end
-
-Shine.Timer.Create( "ServerInfoUpdate", 1, -1, function()
-	local Clients = Shine.GameIDs
-
-	for Client in pairs( Clients ) do
-		local Player = Client:GetControllingPlayer()
-
-		if Player then
-			local Score = Player.score or 0
-			local Name = Player.GetName and Player:GetName() or "unknown"
-
-			Server.UpdatePlayerInfo( Client, Name, Score )
-		end
-	end
-end )
