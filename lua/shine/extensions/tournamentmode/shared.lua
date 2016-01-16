7f2035bacf1adaf209670fda6167bc598f05a5e8
Person8880 <person8880@googlemail.com>
2016-01-16 13:31:45 +0000

    Add translations for the tournament mode plugin
    Also fixes team names not being able to have spaces, and the plugin not
    notifying when a team un-readies in commander only mode.

--- lua/shine/extensions/tournamentmode/shared.lua
+++ lua/shine/extensions/tournamentmode/shared.lua
@@ -20,7 +20,30 @@ function Plugin:SetupDataTable()
 	self:AddDTVar( "integer (0 to 255)", "MarineScore", 0 )
 	self:AddDTVar( "integer (0 to 255)", "AlienScore", 0 )
 
-	self:AddNetworkMessage( "StartNag", { Message = "string (64)" }, "Client" )
+	local TeamField = "integer (1 to 2)"
+
+	self:AddNetworkMessage( "StartNag", { WaitingTeam = "integer (0 to 2)" }, "Client" )
+	self:AddNetworkMessage( "TeamReadyChange", {
+		Team = TeamField,
+		IsReady = "boolean"
+	}, "Client" )
+	self:AddNetworkMessage( "GameStartCountdown", {
+		IsFinalCountdown = "boolean",
+		CountdownTime = "integer"
+	}, "Client" )
+	self:AddNetworkMessage( "GameStartAborted", {}, "Client" )
+	self:AddNetworkMessage( "TeamPlayerNotReady", {
+		Team = TeamField,
+		PlayerName = self:GetNameNetworkField()
+	}, "Client" )
+	self:AddNetworkMessage( "PlayerReadyChange", {
+		PlayerName = self:GetNameNetworkField(),
+		IsReady = "boolean"
+	}, "Client" )
+	self:AddNetworkMessage( "TeamReadyWaiting", {
+		ReadyTeam = TeamField,
+		WaitingTeam = TeamField
+	}, "Client" )
 end
 
 --[[
@@ -28,12 +51,12 @@ end
 ]]
 function Plugin:NetworkUpdate( Key, Old, New )
 	if Server then return end
-	
+
 	--Using team1 or team2 resets the other team's name...
 	if Key == "MarineName" and New ~= "" then
-		Shared.ConsoleCommand( StringFormat( "teams %s %s", New, self.dt.AlienName ) )
+		Shared.ConsoleCommand( StringFormat( "teams \"%s\" \"%s\"", New, self.dt.AlienName ) )
 	elseif Key == "AlienName" and New ~= "" then
-		Shared.ConsoleCommand( StringFormat( "teams %s %s", self.dt.MarineName, New ) )
+		Shared.ConsoleCommand( StringFormat( "teams \"%s\" \"%s\"", self.dt.MarineName, New ) )
 	elseif Key == "MarineScore" then
 		Shared.ConsoleCommand( StringFormat( "score1 %i", New ) )
 	elseif Key == "AlienScore" then
@@ -45,10 +68,79 @@ Shine:RegisterExtension( "tournamentmode", Plugin )
 
 if Server then return end
 
+function Plugin:GetTeamName( Team )
+	if Team == 0 then
+		return self:GetPhrase( "BOTH_TEAMS" )
+	end
+
+	if Team == 1 then
+		return self.dt.MarineName ~= "" and self.dt.MarineName or self:GetPhrase( "MARINES" )
+	end
+
+	return self.dt.AlienName ~= "" and self.dt.AlienName or self:GetPhrase( "ALIENS" )
+end
+
 function Plugin:ReceiveStartNag( Data )
 	local Player = Client.GetLocalPlayer()
-
 	if not Player or not HasMixin( Player, "TeamMessage" ) then return end
-	
-	Player:SetTeamMessage( Data.Message:upper() )
+
+	Player:SetTeamMessage( self:GetInterpolatedPhrase( "WAITING_FOR_TEAM", {
+		TeamName = self:GetTeamName( Data.WaitingTeam )
+	} ) )
+end
+
+function Plugin:Notify( Positive, Message )
+	self:AddChatLine( Positive and 0 or 255, Positive and 255 or 0, 0,
+		self:GetPhrase( "NOTIFY_PREFIX" ), 255, 255, 255, Message )
+end
+
+function Plugin:ReceiveTeamReadyChange( Data )
+	local TranslationKey = Data.IsReady and "TEAM_READY" or "TEAM_NOT_READY"
+
+	self:Notify( Data.IsReady, self:GetInterpolatedPhrase( TranslationKey, {
+			TeamName = self:GetTeamName( Data.Team )
+	} ) )
+end
+
+function Plugin:ReceiveTeamReadyWaiting( Data )
+	self:Notify( true, self:GetInterpolatedPhrase( "TEAM_READY_WAITING", {
+		TeamName = self:GetTeamName( Data.ReadyTeam ),
+		OtherTeamName = self:GetTeamName( Data.WaitingTeam )
+	} ) )
+end
+
+function Plugin:ReceiveGameStartCountdown( Data )
+	local TimeTillStart = Data.CountdownTime
+	local ShouldCountdown = Data.IsFinalCountdown
+
+	local Text = self:GetPhrase( "GAME_START_COUNTDOWN" )
+
+	Shine.ScreenText.Add( "TournamentModeCountdown", {
+		X = 0.5, Y = 0.7,
+		Text = ShouldCountdown and Text or StringFormat( Text, string.TimeToString( TimeTillStart ) ),
+		Duration = 5,
+		R = 255, G = ShouldCountdown and 0 or 255, B = ShouldCountdown and 0 or 255,
+		Alignment = 1,
+		Size = 3,
+		FadeIn = ShouldCountdown and 0 or 1
+	} )
+end
+
+function Plugin:ReceiveGameStartAborted( Data )
+	Shine.ScreenText.Remove( "TournamentModeCountdown" )
+	self:Notify( false, self:GetPhrase( "GAME_START_ABORTED" ) )
+end
+
+function Plugin:ReceiveTeamPlayerNotReady( Data )
+	self:Notify( false, self:GetInterpolatedPhrase( "TEAM_NOT_READY_PLAYER", {
+		TeamName = self:GetTeamName( Data.Team ),
+		PlayerName = Data.PlayerName
+	} ) )
+end
+
+function Plugin:ReceivePlayerReadyChange( Data )
+	local TranslationKey = Data.IsReady and "PLAYER_READY" or "PLAYER_NOT_READY"
+	self:Notify( Data.IsReady, self:GetInterpolatedPhrase( TranslationKey, {
+		PlayerName = Data.PlayerName
+	} ) )
 end
