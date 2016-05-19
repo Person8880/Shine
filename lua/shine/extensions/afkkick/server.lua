e0053bab22ab3327aef2fb0a3eb7967c37825ff4
Person8880 <person8880@googlemail.com>
2016-05-19 17:11:50 +0100

    Add a way to disable the "AFK - " prefix on players

--- lua/shine/extensions/afkkick/server.lua
+++ lua/shine/extensions/afkkick/server.lua
@@ -34,7 +34,8 @@ Plugin.DefaultConfig = {
 	NotifyOnWarn = false,
 	OnlyCheckOnStarted = false,
 	KickOnConnect = false,
-	KickTimeIsAFKThreshold = 0.25
+	KickTimeIsAFKThreshold = 0.25,
+	MarkPlayersAFK = true
 }
 
 Plugin.CheckConfig = true
@@ -99,6 +100,8 @@ do
 
 	function Plugin:PrePlayerInfoUpdate( PlayerInfo, Player )
 		OldFunc = Player.GetName
+
+		if not self.Config.MarkPlayersAFK then return end
 
 		local Client = GetOwner( Player )
 		local Data = self.Users[ Client ]
