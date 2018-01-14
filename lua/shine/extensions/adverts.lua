8f663e8a99ddce9e4e0bf9faa1f40e5b7d7210a5
Person8880 <person8880@googlemail.com>
2018-01-14 14:30:33 +0000

    Add advert templates, trigger-able adverts and gamestate/map filters
    Templates cut down on repetition of values in the config, allowing
    many messages to share the same type/colour/prefix.
    
    Trigger-able adverts live outside the timed cycle and display when
    a certain event happens, such as a round starting or ending.
    
    Gamestate/map filters allow the use of map/state specific adverts.

--- lua/shine/extensions/adverts.lua
+++ lua/shine/extensions/adverts.lua
@@ -5,6 +5,9 @@
 local Shine = Shine
 
 local IsType = Shine.IsType
+local setmetatable = setmetatable
+local StringFormat = string.format
+local StringUpper = string.upper
 local TableQuickCopy = table.QuickCopy
 local TableQuickShuffle = table.QuickShuffle
 local TableRemove = table.remove
@@ -15,19 +18,45 @@ Plugin.Version = "1.2"
 Plugin.PrintName = "Adverts"
 
 Plugin.HasConfig = true
+Plugin.CheckConfig = true
 Plugin.CheckConfigTypes = true
 Plugin.ConfigName = "Adverts.json"
+
+Plugin.AdvertTrigger = table.AsEnum{
+	"COUNTDOWN", "START_OF_ROUND", "END_OF_ROUND",
+	"MARINE_VICTORY", "ALIEN_VICTORY", "DRAW"
+}
+
 Plugin.DefaultConfig = {
+	Templates = {
+		-- Defines named template adverts, allowing many adverts to share the
+		-- same type/colour/prefix.
+		ChatNotification = {
+			Type = "chat",
+			Colour = { 255, 255, 255 },
+			Prefix = "[Info]",
+			PrefixColour = { 0, 200, 255 }
+		}
+	},
+	TriggeredAdverts = {
+		--[[
+		This would trigger a message at the start of a round (after the countdown).
+		{
+			Message = "Good luck and have fun!",
+			Template = "ChatNotification",
+			Trigger = "START_OF_ROUND"
+		}
+		]]
+	},
 	Adverts = {
 		{
 			Message = "Welcome to Natural Selection 2.",
-			Type = "chat",
-			Colour = { 255, 255, 255 }
+			-- Template should match a key in the "Templates" table.
+			Template = "ChatNotification"
 		},
 		{
 			Message = "This server is running the Shine administration mod.",
-			Type = "chat",
-			Colour = { 255, 255, 255 }
+			Template = "ChatNotification"
 		}
 	},
 	Interval = 60,
@@ -57,11 +86,105 @@ Plugin.ConfigMigrationSteps = {
 Plugin.TimerName = "Adverts"
 
 function Plugin:Initialise()
-	self.AdvertsList = TableQuickCopy( self.Config.Adverts )
+	self:ParseAdverts()
 	self:SetupTimer()
 	self.Enabled = true
 
 	return true
+end
+
+function Plugin:OnWebConfigReloaded()
+	self:ParseAdverts()
+	self:SetupTimer()
+end
+
+function Plugin:ParseAdverts()
+	local CurrentMapName = Shared.GetMapName()
+
+	local AdvertList = "Adverts"
+	local function IsValidAdvert( Advert, Index )
+		if not IsType( Advert, "string" ) and not IsType( Advert, "table" ) then
+			self:Print( "%s[ %d ] is neither a string or a table.", true, AdvertList, Index )
+			return false
+		end
+
+		if IsType( Advert, "table" ) and not IsType( Advert.Message, "string" ) then
+			self:Print( "%s[ %d ] has a missing or non-string message.", true, AdvertList, Index )
+			return false
+		end
+
+		return true
+	end
+
+	local function AssignTemplate( Advert )
+		if not IsType( Advert, "table" ) then return Advert end
+
+		local Template = self.Config.Templates[ Advert.Template ]
+
+		if IsType( Template, "table" ) then
+			-- Inherit values from the template.
+			return setmetatable( Advert, { __index = Template } )
+		end
+
+		return Advert
+	end
+
+	self.RequiresGameStateFiltering = false
+
+	-- Parse the list of cycling adverts, filtering out any that are invalid or not enabled
+	-- for the current map.
+	self.AdvertsList = Shine.Stream.Of( self.Config.Adverts )
+		:Map( AssignTemplate )
+		:Filter( IsValidAdvert )
+		:Filter( function( Advert )
+			if not IsType( Advert, "table" ) then return true end
+
+			if Advert.GameState then
+				self.RequiresGameStateFiltering = true
+			end
+
+			if Advert.Maps then
+				return Advert.Maps[ CurrentMapName ]
+			end
+			if Advert.ExcludedMaps then
+				return not Advert.ExcludedMaps[ CurrentMapName ]
+			end
+
+			return true
+		end )
+		:AsTable()
+
+	-- Collect all adverts set to run on a given trigger.
+	local TriggeredAdverts = self.Config.TriggeredAdverts
+	local TriggeredAdvertsByTrigger = Shine.Multimap()
+
+	AdvertList = "TriggeredAdverts"
+	local function IsValidTriggerAdvert( Advert, Index )
+		if not IsValidAdvert( Advert, Index ) then return false end
+
+		if not IsType( Advert, "table" ) or not IsType( Advert.Trigger, "string" ) then
+			self:Print( "%s[ %d ] must have a trigger defined.", true, AdvertList, Index )
+			return false
+		end
+
+		return true
+	end
+
+	Shine.Stream.Of( self.Config.TriggeredAdverts )
+		:Map( AssignTemplate )
+		:Filter( IsValidTriggerAdvert )
+		:ForEach( function( Advert, Index )
+			local TriggerName = StringUpper( Advert.Trigger )
+
+			if self.AdvertTrigger[ TriggerName ] then
+				TriggeredAdvertsByTrigger:Add( TriggerName, Advert )
+			else
+				self:Print( "%s[ %d ] has invalid trigger: %s",
+					true, AdvertList, Index, TriggerName )
+			end
+		end )
+
+	self.TriggeredAdvertsByTrigger = TriggeredAdvertsByTrigger
 end
 
 local function UnpackColour( Colour )
@@ -72,31 +195,13 @@ local function UnpackColour( Colour )
 		tonumber( Colour[ 3 ] ) or 255
 end
 
-function Plugin:ParseAdvert( ID, Advert )
+function Plugin:DisplayAdvert( Advert )
 	if IsType( Advert, "string" ) then
 		Shine:NotifyColour( nil, 255, 255, 255, Advert )
-
-		return true
-	end
-
-	if not IsType( Advert, "table" ) then
-		self:Print( "Misconfigured advert #%i, neither a table nor a string.", true, ID )
-
-		TableRemove( self.AdvertsList, ID )
-
-		return false
+		return
 	end
 
 	local Message = Advert.Message
-	if not IsType( Message, "string" ) then
-		self:Print( "Misconfigured advert #%i, missing or invalid \"Message\" value.",
-			true, ID )
-
-		TableRemove( self.AdvertsList, ID )
-
-		return false
-	end
-
 	local R, G, B = UnpackColour( Advert.Colour )
 	local Type = Advert.Type
 
@@ -107,7 +212,7 @@ function Plugin:ParseAdvert( ID, Advert )
 
  			Shine:NotifyDualColour( nil, PR, PG, PB, Advert.Prefix, R, G, B, Message )
 
- 			return true
+ 			return
 		end
 
 		Shine:NotifyColour( nil, R, G, B, Message )
@@ -130,8 +235,108 @@ function Plugin:ParseAdvert( ID, Advert )
 			Size = 2, FadeIn = 1
 		} )
 	end
+end
 
-	return true
+-- Enums error when accessing a field that is not present...
+local SafeGameStateLookup = {}
+for i = 1, #kGameState do
+	SafeGameStateLookup[ kGameState[ i ] ] = i
+end
+
+--[[
+	Filters the current advert list to those that are valid for the given
+	game state.
+
+	Returns the filtered list, and a boolean to indicate if any have been filtered
+	out.
+]]
+function Plugin:FilterAdvertListForState( Adverts, CurrentAdverts, GameState )
+	local function IsGameState( GameStateName )
+		return SafeGameStateLookup[ GameStateName ] == GameState
+	end
+
+	local function IsValidForGameState( Advert )
+		if not Advert.GameState then return true end
+
+		if IsType( Advert.GameState, "table" ) then
+			for i = 1, #Advert.GameState do
+				if IsGameState( Advert.GameState[ i ] ) then
+					return true
+				end
+			end
+		end
+
+		return IsGameState( Advert.GameState )
+	end
+
+	local OutputAdverts = {}
+	local HasChanged = false
+	for i = 1, #Adverts do
+		if IsValidForGameState( Adverts[ i ] ) then
+			local Index = #OutputAdverts + 1
+			OutputAdverts[ Index ] = Adverts[ i ]
+			if OutputAdverts[ Index ] ~= CurrentAdverts[ Index ] then
+				HasChanged = true
+			end
+		end
+	end
+
+	if #OutputAdverts ~= #CurrentAdverts then
+		HasChanged = true
+	end
+
+	return OutputAdverts, HasChanged
+end
+
+local StateToTrigger = Shine.Multimap( {
+	[ kGameState.Countdown ] = { Plugin.AdvertTrigger.COUNTDOWN },
+	[ kGameState.Started ] = { Plugin.AdvertTrigger.START_OF_ROUND },
+	[ kGameState.Team1Won ] = {
+		Plugin.AdvertTrigger.END_OF_ROUND,
+		Plugin.AdvertTrigger.MARINE_VICTORY
+	},
+	[ kGameState.Team2Won ] = {
+		Plugin.AdvertTrigger.END_OF_ROUND,
+		Plugin.AdvertTrigger.ALIEN_VICTORY
+	},
+	[ kGameState.Draw ] = {
+		Plugin.AdvertTrigger.END_OF_ROUND,
+		Plugin.AdvertTrigger.DRAW
+	}
+} )
+--[[
+	Gets the apprioriate trigger name for the given game state.
+]]
+function Plugin:GetTriggerNamesForGameState( GameState )
+	return StateToTrigger:Get( GameState )
+end
+
+--[[
+	Changes the current advert list based on the new game state, and triggers any
+	triggered adverts.
+]]
+function Plugin:SetGameState( Gamerules, NewState, OldState )
+	if self.RequiresGameStateFiltering then
+		local NewAdverts, HasListChanged = self:FilterAdvertListForState( self.AdvertsList,
+			self.CurrentAdvertsList or self.AdvertsList, NewState )
+		if HasListChanged then
+			-- Reset the advert list to the newly filtered version.
+			self.CurrentMessageIndex = 1
+			self.CurrentAdvertsList = NewAdverts
+		end
+	end
+
+	local Triggers = self:GetTriggerNamesForGameState( NewState )
+	if not Triggers then return end
+
+	for i = 1, #Triggers do
+		local Adverts = self.TriggeredAdvertsByTrigger:Get( Triggers[ i ] )
+		if Adverts then
+			for j = 1, #Adverts do
+				self:DisplayAdvert( Adverts[ j ] )
+			end
+		end
+	end
 end
 
 function Plugin:SetupTimer()
@@ -141,19 +346,26 @@ function Plugin:SetupTimer()
 
 	if #self.AdvertsList == 0 then return end
 
-	local Message = 1
+	self.CurrentMessageIndex = 1
+	if self.RequiresGameStateFiltering then
+		local Gamerules = GetGamerules()
+		local GameState = Gamerules and Gamerules:GetGameState() or kGameState.NotStarted
+		-- Make sure to filter down to the right list of adverts now.
+		self.CurrentAdvertsList = self:FilterAdvertListForState( self.AdvertsList,
+			self.AdvertsList, GameState )
+	else
+		self.CurrentAdvertsList = self.AdvertsList
+	end
 
 	self:CreateTimer( self.TimerName, self.Config.Interval, -1, function()
+		local Message = self.CurrentMessageIndex
 		-- Back to the start, randomise the order again.
 		if Message == 1 and self.Config.RandomiseOrder then
-			TableQuickShuffle( self.AdvertsList )
+			TableQuickShuffle( self.CurrentAdvertsList )
 		end
 
-		if self:ParseAdvert( Message, self.AdvertsList[ Message ] ) then
-			Message = ( Message % #self.AdvertsList ) + 1
-		elseif #self.AdvertsList == 0 then
-			self:DestroyTimer( self.TimerName )
-		end
+		self:DisplayAdvert( self.CurrentAdvertsList[ Message ] )
+		self.CurrentMessageIndex = ( Message % #self.CurrentAdvertsList ) + 1
 	end )
 end
 
---
+++ test/extensions/adverts.lua
@@ -0,0 +1,197 @@
+--[[
+	Adverts plugin test.
+]]
+
+local UnitTest = Shine.UnitTest
+local Adverts = UnitTest:LoadExtension( "adverts" )
+if not Adverts then return end
+
+Adverts = UnitTest.MockOf( Adverts )
+
+Adverts.Config = {
+	Templates = {
+		ChatNotification = {
+			Type = "chat",
+			Colour = { 255, 255, 255 },
+			Prefix = "[Info]",
+			PrefixColour = { 0, 200, 255 }
+		},
+		RoundStarted = {
+			Type = "chat",
+			Colour = { 255, 255, 255 },
+			Prefix = "[Hint]",
+			PrefixColour = { 0, 200, 255 },
+			GameState = "Started"
+		},
+		PreGame = {
+			Type = "chat",
+			Colour = { 255, 255, 255 },
+			Prefix = "[Hint]",
+			PrefixColour = { 0, 200, 255 },
+			GameState = { "NotStarted", "WarmUp" }
+		}
+	},
+	Adverts = {
+		{
+			Message = "This server is running the Shine administration mod.",
+			Template = "ChatNotification"
+		},
+		{
+			Message = "Did you know that Aliens bite things?",
+			Template = "RoundStarted"
+		},
+		{
+			Message = "Get ready to shoot/bite!",
+			Template = "PreGame"
+		},
+		"A string advert.",
+		{
+			-- This is invalid
+			Message = 1
+		},
+		{
+			Message = "I can't be used on any map",
+			Maps = { [ "ns2_invalid" ] = true }
+		},
+		{
+			Message = "I can't be used on the current map",
+			ExcludedMaps = { [ Shared.GetMapName() ] = true }
+		}
+	},
+	TriggeredAdverts = {
+		{
+			Message = "Get zem!",
+			Template = "RoundStarted",
+			Trigger = "START_OF_ROUND"
+		},
+		{
+			Message = "",
+			Trigger = "INVALID"
+		}
+	}
+}
+
+UnitTest:Test( "ParseAdverts parses as expected", function( Assert )
+	Adverts:ParseAdverts()
+
+	Assert:Equals( 4, #Adverts.AdvertsList )
+	local function AssertMatchesTemplate( Advert, Template )
+		for Key, Value in pairs( Template ) do
+			Assert.Equals( "Advert does not match template at key: "..Key, Value, Advert[ Key ] )
+		end
+	end
+	for i = 1, 3 do
+		local Advert = Adverts.AdvertsList[ i ]
+		AssertMatchesTemplate( Advert, Adverts.Config.Templates[ Advert.Template ] )
+	end
+	Assert.Equals( "Expected final advert to be a string", "A string advert.", Adverts.AdvertsList[ 4 ] )
+
+	Assert.DeepEquals( "Triggered advert multimap not mapped as expected", {
+		START_OF_ROUND = {
+			{
+				Message = "Get zem!",
+				Template = "RoundStarted",
+				Trigger = "START_OF_ROUND"
+			}
+		}
+	}, Adverts.TriggeredAdvertsByTrigger:AsTable() )
+	AssertMatchesTemplate( Adverts.TriggeredAdvertsByTrigger:Get( "START_OF_ROUND" )[ 1 ], Adverts.Config.Templates.RoundStarted )
+end )
+
+UnitTest:Test( "FilterAdvertListForState filters when adverts should be", function( Assert )
+	local AdvertsList = {
+		{
+			Message = "This server is running the Shine administration mod.",
+		},
+		{
+			Message = "Did you know that Aliens bite things?",
+			GameState = "Started"
+		},
+		{
+			Message = "Get ready to shoot/bite!",
+			GameState = { "NotStarted", "WarmUp" }
+		}
+	}
+
+	local Filtered, HasListChanged = Adverts:FilterAdvertListForState( AdvertsList, AdvertsList, kGameState.Started )
+	Assert.True( "Expected list to be marked as changed", HasListChanged )
+	Assert.ArrayEquals( "Expected only adverts valid for gamestate",
+		{ AdvertsList[ 1 ], AdvertsList[ 2 ] }, Filtered )
+end )
+
+UnitTest:Test( "FilterAdvertListForState produces same list when all match", function( Assert )
+	local AdvertsList = {
+		{
+			Message = "This server is running the Shine administration mod.",
+		},
+		{
+			Message = "Did you know that Aliens bite things?",
+			GameState = "Started"
+		},
+		{
+			Message = "Get ready to shoot/bite!"
+		}
+	}
+	local Filtered, HasListChanged = Adverts:FilterAdvertListForState( AdvertsList, AdvertsList, kGameState.Started )
+	Assert.False( "Expected list to not be marked as changed", HasListChanged )
+	Assert.ArrayEquals( "Expected output to be identical to input", AdvertsList, Filtered )
+end )
+
+Adverts.RequiresGameStateFiltering = false
+local Displayed = {}
+function Adverts:DisplayAdvert( Advert )
+	Displayed[ Advert ] = true
+end
+
+Adverts.TriggeredAdvertsByTrigger = Shine.Multimap( {
+	[ Adverts.AdvertTrigger.START_OF_ROUND ] = {
+		{
+			Message = "1"
+		},
+		{
+			Message = "2"
+		}
+	}
+} )
+
+UnitTest:Test( "SetGameState - Displays adverts for triggers", function( Assert )
+	Adverts:SetGameState( nil, kGameState.Started, kGameState.Countdown )
+
+	local AdvertsTriggered = Adverts.TriggeredAdvertsByTrigger:Get( Adverts.AdvertTrigger.START_OF_ROUND )
+	for i = 1, #AdvertsTriggered do
+		Assert.True( "Advert should have been triggered", Displayed[ AdvertsTriggered[ i ] ] )
+	end
+end )
+
+Adverts.RequiresGameStateFiltering = true
+Adverts.AdvertsList = {
+	{
+		Message = "1"
+	},
+	{
+		Message = "2"
+	}
+}
+Adverts.CurrentAdvertsList = Adverts.AdvertsList
+Adverts.CurrentMessageIndex = 2
+
+function Adverts:FilterAdvertListForState()
+	return self.AdvertsList, false
+end
+
+UnitTest:Test( "SetGameState - Does nothing if no triggers or filter change", function( Assert )
+	Adverts:SetGameState( nil, kGameState.Countdown, kGameState.PreGame )
+	Assert.Equals( "Advert list should not have changed", Adverts.AdvertsList, Adverts.CurrentAdvertsList )
+	Assert.Equals( "Advert index should not have changed", 2, Adverts.CurrentMessageIndex )
+end )
+
+Adverts.CurrentAdvertsList = {}
+function Adverts:FilterAdvertListForState()
+	return self.AdvertsList, true
+end
+
+UnitTest:Test( "SetGameState - Updates current advert list on filter change", function( Assert )
+	Adverts:SetGameState( nil, kGameState.Countdown, kGameState.PreGame )
+	Assert.Equals( "Advert list should have changed", Adverts.AdvertsList, Adverts.CurrentAdvertsList )
+	Assert.Equals( "Advert index should have reset", 1, Adverts.CurrentMessageIndex )
+end )
