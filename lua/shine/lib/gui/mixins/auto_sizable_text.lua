3980b9688b0ae3e829373d8fe9617a0da89584cf
Person8880 <person8880@googlemail.com>
2018-04-26 15:12:54 +0100

    Remove line counting from text height computation
    The native method accounts for new lines already.

--- lua/shine/lib/gui/mixins/auto_sizable_text.lua
+++ lua/shine/lib/gui/mixins/auto_sizable_text.lua
@@ -2,8 +2,6 @@
 	Provides auto-sizing logic based on element text.
 ]]
 
-local StringGMatch = string.gmatch
-
 local AutoSizeText = {}
 
 function AutoSizeText:GetContentSizeForAxis( Axis )
@@ -29,18 +27,7 @@ function AutoSizeText:GetTextHeight( Text )
 	local Scale = self.TextScale
 	Scale = Scale and Scale.y or 1
 
-	if Text then
-		return self.Label:GetTextHeight( Text ) * Scale
-	end
-
-	local Lines = 1
-	Text = self.Label:GetText()
-
-	for Match in StringGMatch( Text, "\n" ) do
-		Lines = Lines + 1
-	end
-
-	return self.Label:GetTextHeight( "!" ) * Lines * Scale
+	return self.Label:GetTextHeight( Text or self.Label:GetText() ) * Scale
 end
 
 Shine.GUI:RegisterMixin( "AutoSizeText", AutoSizeText )
