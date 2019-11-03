c3631a8ab5674885eb46df929ef786d36e946ed7
Person8880 <person8880@googlemail.com>
2019-11-03 12:59:03 +0000

    Ensure player names are unique when adding AFK prefix
    Make sure that players are not able to name themselves with the AFK
    prefix, and sanity check on the client that adding the prefix creates
    a new unique name.
    
    This allows the scoreboard name index to be updated without one player's
    data potentially overwriting another's.

--- lua/shine/extensions/afkkick/client.lua
+++ lua/shine/extensions/afkkick/client.lua
@@ -4,6 +4,8 @@
 
 local Plugin = ...
 
+local setmetatable = setmetatable
+local StringFormat = string.format
 local xpcall = xpcall
 
 Plugin.NotifySoundEffect = "sound/NS2.fev/common/tooltip_on"
@@ -14,18 +16,43 @@ function Plugin:ReceiveAFKNotify( Data )
 end
 
 function Plugin:SetupAFKScoreboardPrefix()
+	local GetPlayerDataByName = Shine.GetUpValueAccessor( Scoreboard_ReloadPlayerData, "playerDataByName", {
+		Recursive = true,
+		Predicate = Shine.UpValuePredicates.DefinedInFile( "lua/Scoreboard.lua" )
+	} )
+
 	Shine.Hook.SetupGlobalHook( "Scoreboard_ReloadPlayerData",
 		"PostScoreboardReload", "PassivePost" )
 
-	local AFK_PREFIX = "AFK - "
+	local AFK_PREFIX = self.AFK_PREFIX
 	local CALLING = false
 
 	local function UpdateNamesWithAFKState()
+		local PlayerDataByName = GetPlayerDataByName()
+
+		local UniqueNames = setmetatable( {}, { __index = PlayerDataByName } )
 		local EntityList = Shared.GetEntitiesWithClassname( "PlayerInfoEntity" )
 		for _, Entity in ientitylist( EntityList ) do
 			local Entry = Scoreboard_GetPlayerRecord( Entity.clientId )
-			if Entry and Entry.Name and Entity.afk then
-				Entry.Name = AFK_PREFIX..Entry.Name
+			if Entry and Entry.Name then
+				if Entity.afk then
+					local NewName = AFK_PREFIX..Entry.Name
+
+					-- Make sure the new name is unique.
+					local Index = 2
+					while UniqueNames[ NewName ] and UniqueNames[ NewName ] ~= Entry do
+						NewName = StringFormat( "%s%s (%s)", AFK_PREFIX, Entry.Name, Index )
+						Index = Index + 1
+					end
+
+					Entry.Name = NewName
+
+					if PlayerDataByName then
+						PlayerDataByName[ NewName ] = Entry
+					end
+				end
+
+				UniqueNames[ Entry.Name ] = Entry
 			end
 		end
 	end
--- lua/shine/extensions/afkkick/shared.lua
+++ lua/shine/extensions/afkkick/shared.lua
@@ -5,6 +5,8 @@
 local Plugin = Shine.Plugin( ... )
 Plugin.NotifyPrefixColour = { 255, 50, 0 }
 
+Plugin.AFK_PREFIX = "AFK - "
+
 function Plugin:SetupDataTable()
 	self:AddNetworkMessage( "AFKNotify", {}, "Client" )
 	self:AddTranslatedNotify( "WARN_KICK_ON_CONNECT", {
