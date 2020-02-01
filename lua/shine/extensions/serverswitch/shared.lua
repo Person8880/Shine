0e88c6a227dce0e2a3ae597d2507ad36e0a56c02
Person8880 <person8880@googlemail.com>
2020-02-01 14:18:10 +0000

    Update server switch networking to remove delay
    Also, add config validation and fix enabling mid-round not sending
    server data to clients.

--- lua/shine/extensions/serverswitch/shared.lua
+++ lua/shine/extensions/serverswitch/shared.lua
@@ -8,7 +8,7 @@ function Plugin:SetupDataTable()
 	self:AddNetworkMessage( "ServerList", {
 		Name = "string (15)",
 		IP = "string (16)",
-		Port = "string (6)",
+		Port = "integer",
 		ID = "integer (0 to 255)"
 	}, "Client" )
 end
