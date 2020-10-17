58c42ece31e937e05b100e214cff1bd329906b30
Person8880 <person8880@googlemail.com>
2020-10-17 11:56:42 +0100

    Fix arguments passed to width unit in spacer

--- lua/shine/lib/gui/richtext/elements/spacer.lua
+++ lua/shine/lib/gui/richtext/elements/spacer.lua
@@ -15,14 +15,14 @@ end
 
 function Spacer:Split( Index, TextSizeProvider, Segments, MaxWidth )
 	self.OriginalElement = Index
-	self.Width = self.AutoWidth:GetValue( MaxWidth, 1 )
+	self.Width = self.AutoWidth:GetValue( MaxWidth, nil, 1 )
 	self.WidthWithoutSpace = self.IgnoreOnNewLine and 0 or self.Width
 
 	Segments[ #Segments + 1 ] = self
 end
 
 function Spacer:GetWidth( TextSizeProvider, MaxWidth )
-	local Width = self.AutoWidth:GetValue( MaxWidth, 1 )
+	local Width = self.AutoWidth:GetValue( MaxWidth, nil, 1 )
 	return Width, self.IgnoreOnNewLine and 0 or Width
 end
 
