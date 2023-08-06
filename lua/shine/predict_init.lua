78b9670e22535e06c6a3deb7c3977b14a6329768
Person8880 <person8880@googlemail.com>
2023-08-06 19:08:12 +0100

    Clean up init scripts

--- lua/shine/predict_init.lua
+++ lua/shine/predict_init.lua
@@ -2,16 +2,11 @@
 	Shine prediction VM startup.
 ]]
 
-local include = Script.Load
-local StringFormat = string.format
-
 -- Load the minimal scripts required to enable extensions, nothing other than game logic is required in this VM.
-local Scripts = {
+Shine.LoadScripts( {
 	"core/shared/misc.lua",
 	"core/shared/logging.lua",
 	"core/shared/config.lua",
 	"lib/datatables.lua",
 	"core/shared/extensions.lua"
-}
-
-Shine.LoadScripts( Scripts )
+} )
