1efe7d0ddfa2efa39b3464cd62798193a34b6863
Person8880 <person8880@googlemail.com>
2023-11-04 14:05:02 +0000

    Suppress errors in predict VM log messages
    These are superfluous and cause stuttering.

--- lua/shine/predict_init.lua
+++ lua/shine/predict_init.lua
@@ -2,6 +2,18 @@
 	Shine prediction VM startup.
 ]]
 
+Shine.Hook.CallAfterFileLoad( "lua/DebugUtility.lua", function()
+	local OldLog = _G.Log
+	if Shine.IsType( OldLog, "function" ) then
+		local pcall = pcall
+		function Log( FormatString, ... )
+			-- Suppress errors in log output to stop prediction VM stutter. The game has some broken log statements
+			-- that were never tested.
+			pcall( OldLog, FormatString, ... )
+		end
+	end
+end )
+
 -- Load the minimal scripts required to enable extensions, nothing other than game logic is required in this VM.
 Shine.LoadScripts( {
 	"core/shared/misc.lua",
